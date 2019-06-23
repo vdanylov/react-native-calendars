84fa9daf83e6da05bb66f2b2a12d4903f3c5ccb6
Karol Szurmański <devszurmanski@gmail.com>
2019-06-23 17:01:31 +0200

    added year in header

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -214,7 +214,7 @@ export default class AgendaView extends Component {
 
   onVisibleMonthsChange(months) {
     this.setState({
-      currentVisibleMonth: parseDate(months[0]).toString("MMMM")
+      currentVisibleMonth: parseDate(months[0]).toString("MMMM yyyy")
     });
     if (this.props.items && !this.state.firstResevationLoad) {
       clearTimeout(this.scrollTimeout);
