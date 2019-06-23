c9bce3aee62c8cb8f64b4a910dfddf3adf35830f
Karol Szurmański <devszurmanski@gmail.com>
2019-06-23 14:43:31 +0200

    added custom icons for custom-period day

--- src/calendar/day/custom-period/index.js
+++ src/calendar/day/custom-period/index.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { PureComponent, Fragment } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import styleConstructor from "./style";
 
 class Day extends PureComponent {
@@ -7,6 +7,22 @@ class Day extends PureComponent {
     super(props);
     this.style = styleConstructor(props.theme);
   }
+  getIconSource = icon => {
+    switch (icon) {
+      case "PASTDAY":
+        return require("./icons/iconStar.png");
+      case "PASTDAYACTIVE":
+        return require("./icons/iconStarActive.png");
+      case "DELIVERYDAY":
+        return require("./icons/iconDelivery.png");
+      case "FUTUREDAY":
+        return require("./icons/iconMenu.png");
+      case "FUTUREDAYACTIVE":
+        return require("./icons/iconMenuActive.png");
+      default:
+        return null;
+    }
+  };
 
   render() {
     const {
@@ -19,54 +35,63 @@ class Day extends PureComponent {
         today,
         singleDay,
         color,
-        disabled
+        disabled,
+        icon
       },
       onPress
     } = this.props;
     const isSelectedInAnyOption =
       selected || startingDay || endingDay || today || singleDay;
-
+    const iconSource = this.getIconSource(icon);
     const backgroundColor = {
       backgroundColor: color ? color : null
     };
     return (
-      <TouchableOpacity
-        disabled={disabled}
-        onPress={() => onPress(date.dateString)}
-        style={this.style.dayWrapper}
-      >
-        <View style={this.style.dayWrapper}>
-          <View
-            style={[
-              this.style.background,
-              selected && this.style.backgroundSelected,
-              startingDay && this.style.backgroundStartingDay,
-              endingDay && this.style.backgroundEndingDay,
-              !singleDay && backgroundColor
-            ]}
-          />
-          <View
-            style={[
-              this.style.textWrapper,
-              singleDay && this.style.textWrapperSingleDay,
-              today && this.style.textWrapperToday,
-              singleDay && backgroundColor
-            ]}
-          >
-            <Text
+      <Fragment>
+        <TouchableOpacity
+          disabled={disabled}
+          onPress={() => onPress(date.dateString)}
+          style={this.style.dayWrapper}
+        >
+          <View style={this.style.dayWrapper}>
+            <View
               style={[
-                this.style.text,
-                isSelectedInAnyOption
-                  ? this.style.textSelected
-                  : this.style.textNotSelected,
-                today && this.style.textToday
+                this.style.background,
+                selected && this.style.backgroundSelected,
+                startingDay && this.style.backgroundStartingDay,
+                endingDay && this.style.backgroundEndingDay,
+                !singleDay && backgroundColor
+              ]}
+            />
+            <View
+              style={[
+                this.style.textWrapper,
+                singleDay && this.style.textWrapperSingleDay,
+                today && this.style.textWrapperToday,
+                singleDay && backgroundColor
               ]}
             >
-              {date.day}
-            </Text>
+              <Text
+                style={[
+                  this.style.text,
+                  isSelectedInAnyOption
+                    ? this.style.textSelected
+                    : this.style.textNotSelected,
+                  today && this.style.textToday
+                ]}
+              >
+                {date.day}
+              </Text>
+            </View>
           </View>
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+        {iconSource ? (
+          <Image source={iconSource} style={{ marginTop: 5 }} />
+        ) : (
+          {/* placeholder */}
+          <View style={{ height: 18 }} />
+        )}
+      </Fragment>
     );
   }
 }
